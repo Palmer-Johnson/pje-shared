42a41c62193225977051f3f86eb056115862d44b
Dustin Walker <me@dustinwalker.com>
2022-11-23 14:24:32 -0600

    ADD: Shared content purge

---
+++ reference/tailwind-content.js
@@ -0,0 +1,5 @@
+module.exports = [
+  './vendor/astuteo/pje-shared/src/templates/**/*.twig',
+  './vendor/wabisoft/components/src/templates/**/*.twig',
+  './vendor/wabisoft/components/src/models/**/*.php',
+]
